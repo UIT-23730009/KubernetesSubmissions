9afb9f12fdb833d8d15ec7268fa816f81e46391f
UIT-23730009 <149002453+UIT-23730009@users.noreply.github.com>
2025-11-10 14:33:22 +0000

    feat: sync frontend version

--- bin/sync-version.sh
+++ bin/sync-version.sh
@@ -11,6 +11,10 @@ jq --arg ver "$VERSION" '.version = $ver' ./log_output/package-lock.json > tmp.$
 jq --arg ver "$VERSION" '.version = $ver' ./the_project/todo-app/package.json > tmp.$$.json && mv tmp.$$.json ./the_project/todo-app/package.json
 jq --arg ver "$VERSION" '.version = $ver' ./the_project/todo-app/package-lock.json > tmp.$$.json && mv tmp.$$.json ./the_project/todo-app/package-lock.json
 
+#sync version with /the_project/todo-frontend
+jq --arg ver "$VERSION" '.version = $ver' ./the_project/todo-frontend/package.json > tmp.$$.json && mv tmp.$$.json ./the_project/todo-frontend/package.json
+jq --arg ver "$VERSION" '.version = $ver' ./the_project/todo-frontend/package-lock.json > tmp.$$.json && mv tmp.$$.json ./the_project/todo-frontend/package-lock.json
+
 git add ./log_output/package.json
 git add ./the_project/todo-app/package.json 
 git commit -m "chore: sync version to $VERSION in package.json"
